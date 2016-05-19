55d0026ea8b1eb2954c1a76459f3bb8a73285ab7
gampeixoto <gabriel.a.m.peixoto@hotmail.com>
2016-05-18 22:02:33 -0300

    Adding the basic settings for the PaaS

--- server.js
+++ server.js
@@ -2,7 +2,8 @@
 
 var http = require('http');
 var app = require('./config/express')();
-require('./config/database.js')('mongodb://localhost/vacinalegal');
+require('./config/database.js')(process.env.MONGOLAB_URI ||
+    'mongodb://test:t3st@ds025772.mlab.com:25772/vacinalegal_db');
 
 http.createServer(app).listen(app.get('port'), function () {
     console.log('Express Server escutando na porta ' + app.get('port'));
